3676d2904b1e1d70262bca63afac0de00485d2c4
Dominik Zarsky <dominik.zarsky@gmail.com>
2021-09-13 17:56:22 +0200

    add pdf merger, update swagger info

---
+++ src/func/pdfmerger.py
@@ -0,0 +1,39 @@
+from PyPDF2 import PdfFileMerger, PdfFileReader
+from fastapi import UploadFile
+from typing import List
+from os import path, mkdir, remove
+import uuid
+
+tempdir = '/tmp/merger'
+
+
+async def merge(files: List[UploadFile]) -> str:
+    merger = PdfFileMerger()
+    filename = '{}.pdf'.format(uuid.uuid1())
+    tempfiles = []
+
+    if not path.exists(tempdir):
+        mkdir(tempdir)
+
+    await save_files(files)
+
+    for file in files:
+        merger.append(PdfFileReader('{}/{}'.format(tempdir, file.filename)))
+        tempfiles.append(file.filename)
+
+    await clean_files(tempfiles)
+
+    return_path = '{}/{}'.format(tempdir, filename)
+    merger.write(return_path)
+    return return_path
+
+
+async def save_files(files: List[UploadFile]):
+    for file in files:
+        with open('{}/{}'.format(tempdir, file.filename), 'wb+') as temp:
+            temp.write(file.file.read())
+
+
+async def clean_files(files: List[str]):
+    for filename in files:
+        remove('{}/{}'.format(tempdir, filename))
--- src/main.py
+++ src/main.py
@@ -1,13 +1,38 @@
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.responses import FileResponse
-from func import ocr
+from typing import List
+from func import ocr, pdfmerger
 
-app = FastAPI()
+app = FastAPI(
+    title='PDF REST',
+    description='REST API for manipulating PDF files',
+    version='1.0.0',
+    license_info={
+        'name': 'MIT',
+        'url': 'https://github.com/Loupeznik/pdf-rest/blob/master/LICENSE'
+    }
+)
+
+responses = {
+    400: {'error': 'Bad file type'},
+    200: {'content': {'application/pdf': {}}}
+}
 
 
-@app.post('/ocr-pdf')
-async def create_upload_files(file: UploadFile = File(...)):
+@app.post('/ocr-pdf', name='Run OCR', description='Performs OCR on a file', responses={**responses})
+async def ocr(file: UploadFile = File(...)):
     if file.content_type != 'application/pdf':
         raise HTTPException(status_code=400, detail='Bad file type')
     filepath = await ocr.scan(file)
+    return FileResponse(filepath)
+
+
+@app.post('/merge-pdf', name='Merge multiple files', description='Merge multiple PDF files', responses={**responses})
+async def merge(files: List[UploadFile] = File(...)):
+    for file in files:
+        if file.content_type != 'application/pdf':
+            raise HTTPException(status_code=400, detail='Bad file type')
+        else:
+            continue
+    filepath = await pdfmerger.merge(files)
     return FileResponse(filepath)
